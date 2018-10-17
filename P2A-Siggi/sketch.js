c72a2685dbae1227a1f6534a64fac675d2d8d85a
siggikp <43656251+siggikp@users.noreply.github.com>
2018-10-17 10:40:22 +0000

    up

--- P2A-Siggi/sketch.js
+++ P2A-Siggi/sketch.js
@@ -1,1 +1,10 @@
 // Hér kemur kóðinn þinn:
+function setup(){
+    createCanvas(1024,1024);
+    background(255,0,0);
+    fill(150, 200, 10);
+    ellipse(200,100,150,50);
+    fill(0);
+    textSize(40);
+    text("Halló heimur",200,40);
+}
