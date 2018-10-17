3f26d5d8e7bf2a501f44d88f122d0a20048a3608
siggikp <43656251+siggikp@users.noreply.github.com>
2018-10-17 11:07:09 +0000

    up

--- P2A-Siggi/sketch.js
+++ P2A-Siggi/sketch.js
@@ -1,8 +1,11 @@
 var faceWidth = 100;
 var faceHeight = 150;
 var eyeSize = 20;
+var auga1haed = 30;
+var auga1breidd = 30;
 var x = 150;
 var y = 150;
+
 
 function setup(){
   createCanvas (300,300);
@@ -11,7 +14,8 @@ function setup(){
 function draw(){
   background(180);
   ellipse(x, y, faceWidth, faceHeight);
-  ellipse (x, y, eyeSize)
+  fill(255)
+  ellipse(x, y, auga1haed, auga1breidd);
 
   //.....kóði sem staðsetur augun m.v. x, y, faceWidth og faceHeight ætti að koma hér.
 
@@ -21,6 +25,8 @@ function mousePressed(){
   //Í hvert sinn sem músinni er smellt fá breyturnar ný gildi.
   faceWidth  = random(75,  150);
   faceHeight = random(100, 200);
+  auga1haed = random(20, 30);
+  auga1breidd = random(20, 30);
   eyeSize    = random(10,  30);
 
 }
