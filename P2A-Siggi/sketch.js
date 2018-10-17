dfa812782c34b478fd29848c69cbe40e3481d956
siggikp <43656251+siggikp@users.noreply.github.com>
2018-10-17 10:58:25 +0000

    upp

--- P2A-Siggi/sketch.js
+++ P2A-Siggi/sketch.js
@@ -1,26 +1,24 @@
+var eyeSize = 20;
+var faceWidth = 100;
+var faceHeight = 150;
+var x = 150;
+var y = 150;
 
 function setup(){
-    createCanvas(598,597);
-    background(236, 238, 227);
-    fill (195, 49, 36)
-    rect(0, 0, 254, 322)
-    fill (32, 73, 135)
-    rect(527, 335, 67, 95)
-    fill (242, 213, 50)
-    rect(265, 577, 155, 20)
-    fill (35, 36, 38)
-    rect(433, 577, 90, 20)
-//Svörtu línurnar
-    fill (0, 0, 0)
-    rect(255, 0, 14, 597)
-    fill(0, 0, 0)
-    rect (511, 322, 15, 275)
-    fill(0, 0, 0)
-    rect(0, 322, 594, 14)
-    fill(0, 0, 0)
-    rect(520, 430, 75, 19)
-    fill (0, 0, 0)
-    rect(419, 567, 13, 34)
-    fill (0, 0, 0)
-    rect(266, 564, 260, 14)
+  createCanvas (300,300);
+}
+
+function draw(){
+  background(180);
+  ellipse(x, y, faceWidth, faceHeight);
+
+  //.....kóði sem staðsetur augun m.v. x, y, faceWidth og faceHeight ætti að koma hér.
+
+}
+
+function mousePressed(){
+  //Í hvert sinn sem músinni er smellt fá breyturnar ný gildi.
+  faceWidth  = random(75,  150);
+  faceHeight = random(100, 200);
+  eyeSize    = random(10,  30);
 }
