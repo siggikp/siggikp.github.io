a87107037548c0eb18793041d3f197703f257961
siggikp <43656251+siggikp@users.noreply.github.com>
2018-10-31 08:56:00 +0000

    up

--- P3A-KazemirMalevich/sketch.js
+++ P3A-KazemirMalevich/sketch.js
@@ -1,9 +1,9 @@
 function setup(){
-  createCanvas(300, 300);
+  createCanvas(300, 240);
   fill(237,239,226);
   background(240, 242, 233);
   fill(20, 23, 21);
   rect(27, 6, 262, 158);
   fill(36,75,154);
-  triangle(180, 72, 118, 215, 263, 216);
+  triangle(180, 72, 118, 215, 263, 215);
 }
