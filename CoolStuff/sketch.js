e6ce5ab7b6ea961713bba94ca6ac0c40b17e71d2
siggikp <43656251+siggikp@users.noreply.github.com>
2018-10-31 08:41:53 +0000

    up

--- CoolStuff/sketch.js
+++ CoolStuff/sketch.js
@@ -1,14 +1,6 @@
 // Hér kemur kóðinn þinn:
 function setup(){
-    createCanvas(1024,1024);
-    background(255,0,0);
-    fill(150, 200, 10);
-    ellipse(200,100,150,50);
-    fill(0);
-    textSize(40);
-    text("Halló heimur",200,40);
-    fill (244, 66, 66)
-    rect(745, 197, 20, 60)
-    fill(66, 134, 244)
-    triangle(558, 359, 482, 452, 842, 384)
+createCanvas(300, 300);
+for(var i = 0; i < 5; i = i + 1);
+text(""+i, width/2, 20+i*10;)
 }
