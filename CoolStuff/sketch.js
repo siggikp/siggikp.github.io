300fbf0493a5b5a598f8f123b48e8ef5e66f5899
siggikp <43656251+siggikp@users.noreply.github.com>
2018-10-31 08:58:45 +0000

    up

--- CoolStuff/sketch.js
+++ CoolStuff/sketch.js
@@ -1,5 +1,6 @@
 function setup(){
   createCanvas(300, 300);
-  for(var i = 0; i < 5; i = i + 1);
-  text(""+i, width/2, 20+i*10);
+  for(var i = 0; i < 5; i = i + 1); {
+    text(""+i, width/2, 20+i*10);
+  }
 }
