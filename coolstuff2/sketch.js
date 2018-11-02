40b69226cb69d2f7ac10c984d1fe4006a7403848
siggikp <43656251+siggikp@users.noreply.github.com>
2018-11-02 13:13:58 +0000

    up

---
+++ coolstuff2/sketch.js
@@ -0,0 +1,18 @@
+var stepSize = 10
+
+function setup() {
+  createCanvas(400,400);
+  background(255, 215, 0);
+  fill(0);
+  colorMode(HSB);
+  frameRate(5);
+}
+
+function draw(){
+  for(var i = 0; i < random(1, 20) ; i = i + 1) {
+    fill(random(0,360),75,100);
+    for(var a = 0; a < 21 ; a = a + 1) {
+      ellipse(10 + i*20, 10 + a*20, 15, 15);
+    }
+  }
+}
