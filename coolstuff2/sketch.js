bd43834f21bdf48de3abf8d06e3c091a941a830b
siggikp <43656251+siggikp@users.noreply.github.com>
2018-11-05 11:44:58 +0000

    up

--- coolstuff2/sketch.js
+++ coolstuff2/sketch.js
@@ -12,7 +12,7 @@ function draw(){
   background(255, 215, 0);
   for(var i = 0; i < 21 ; i = i + 1) {
     fill(random(0,360),75,100);
-    for(var a = 0; a < random(1, 21) ; a = a + 1) {
+    for(var a = 0; a < mouseY(1, 21) ; a = a + 1) {
       ellipse(10 + i*20, 10 + a*20, 15, 15);
         for(var d = 0; d < 21 ; d = d + 1) {
       rect(0 + i*20, 0 + d, 20, 20)
