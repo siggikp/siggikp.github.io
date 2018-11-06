b9c14a10d1821c396ece8feb35f79893649ada3e
siggikp <43656251+siggikp@users.noreply.github.com>
2018-11-06 08:21:28 +0000

    up

--- p4-skjaskipting/sketch.js
+++ p4-skjaskipting/sketch.js
@@ -1,11 +1,20 @@
-function setup() {
-  createCanvas(400,400);
-  background(255, 215, 0);
-  fill(0);
-}
-function draw(){
-  if(mouseX < 200){
-    ellipse(mouseX, mouseY, 30, 30);
-  }
-}
-}
+    function setup() {
+      createCanvas(400,400);
+
+      fill(0);
+    }
+    function draw(){
+      background(255, 215, 0);
+      if(mouseX < 200 && mouseY < 200){
+        rect(0, 0, 200, 200);
+    }
+        if(mouseX > 200 && mouseY < 200){
+          rect(200, 0, 200, 200);
+        }
+          if(mouseX > 200 && mouseY > 200){
+           rect(200, 200, 200, 200);
+          }
+          if(mouseX < 200 && mouseY > 200){
+           rect(0, 200, 200, 200);
+          }
+    }
