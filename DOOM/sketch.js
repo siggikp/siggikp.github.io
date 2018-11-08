17260f07a833ec95d8c78b146d0e050dce81d896
siggikp <43656251+siggikp@users.noreply.github.com>
2018-11-08 10:37:58 +0000

    up

--- DOOM/sketch.js
+++ DOOM/sketch.js
@@ -3,6 +3,6 @@ var dosbox = new Dosbox({
        onload: function (dosbox) {   dosbox.run("https://cdn.rawgit.com/darrengruber/docker-em-dosbox-doom/afad47b2/doom_shareware/doom19s_deiced.zip", "./DOOM.EXE");
        },
        onrun: function (dosbox, app) {
-         console.log("App '" + app + "' is runned");
+         console.log("App '" + app + "' is runned :)");
        }
      });
