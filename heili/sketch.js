a2d4d3c94451f88aed3bfc141a5b434423d2c38e
siggikp <43656251+siggikp@users.noreply.github.com>
2018-11-09 13:15:32 +0000

    up

--- heili/sketch.js
+++ heili/sketch.js
@@ -1,7 +1,7 @@
 var viti;
 
 function setup() {
-  viti = createImg('https://en.wikipedia.org/wiki/Political_compass#/media/File:Political_chart.svg');
+  viti = createImg('https://imgur.com/BUjjzd2');
   createCanvas(1200,1306);
   background(viti);
 }
