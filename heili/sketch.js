461795aa70c22b6e98dad4da29615cc371e01781
siggikp <43656251+siggikp@users.noreply.github.com>
2018-11-09 13:12:06 +0000

    up

--- heili/sketch.js
+++ heili/sketch.js
@@ -1,14 +1,7 @@
 var viti;
 
-function preload() {
-  viti = createImg('https://en.wikipedia.org/wiki/Political_compass#/media/File:Political_chart.svg');
-}
-
 function setup() {
+  viti = createImg('https://en.wikipedia.org/wiki/Political_compass#/media/File:Political_chart.svg');
   createCanvas(1200,1306);
   background(viti);
-}
-
-function draw() {
-  ellipse (random(0,width),random(0,height),10,10);
 }
