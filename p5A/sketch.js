2c77bd05c5f514fe9b4ec6bd446ed55aa0f98588
siggikp <43656251+siggikp@users.noreply.github.com>
2018-11-30 13:27:40 +0000

    up

--- p5A/sketch.js
+++ p5A/sketch.js
@@ -1,37 +1,43 @@
-var x = 0;
-var y = 0;
-var xSpeed = 8;
-var ySpeed = 4;
-var hreyf = 0;
-var spadibreidd = 100;
-var spadiY = 400
+var x = 100;
+var y = 100;
+var hradiX = 5;
+var hradiY = 11;
+var boltiStaerd = 20;
+var stig = 0;
+var spadiBreidd = 150;
+var spadiThykkt = 25;
+var spadiY;
 
 function setup() {
-  createCanvas(500,500);
+	createCanvas(600,500);
+	spadiY = height - 45;
+	rectMode(CENTER);
+	textFont("Courier New",16);
+	textStyle(BOLD);
 }
 
 function draw() {
   background(255,200,0);
+	// Reikna ný hnit út frá hraða boltans:
+  x = x + hradiX;
+  y = y + hradiY;
+	// Athuga hvort boltinn snertir veggi:
+  if ((x > width-boltiStaerd/2) || (x < boltiStaerd/2))  {
+    hradiX = hradiX * -1;
+  }
+  if ((y > height-boltiStaerd/2) || (y < boltiStaerd)) {
+    hradiY = hradiY * -1;
+  }
+	// Athuga hvort bolti snertir spaða:
+	// abs() táknar algildi/tölugildi
+	if(abs(spadiY - y) < boltiStaerd/2 + spadiThykkt/2 && abs(mouseX - x) < boltiStaerd/2 + spadiBreidd/2){
+    hradiY = hradiY * -1;
+		stig = stig + 1;
+	}
+	// Teikna boltann, spaðann og stigin
   fill(236,36,94);
-  rect(hreyf, spadiY, spadibreidd, 30);
-  fill(236,36,94);
-  ellipse(x, y, 30, 30);
-  x = x + xSpeed;
-  y = y +ySpeed;
-  if ((x > width) || (x < 0)) {
-    xSpeed = xSpeed * -1;
-  }
-  hreyf = mouseX
-  if (hreyf > width - spadibreidd/2) {
-    hreyf = width - spadibreidd/2;
-  }
-  if ((y > height) || (y < 0)) {
-    ySpeed = ySpeed * -1;
-  }
-  spadibreidd = 100
-  spadiY = 400
-  hreyf = mouseX
-  if(abs(spadiY - y) < 30/2 + 30/2 && abs(hreyf - x) < 30/2 + 30/2){
-  ySpeed = ySpeed * -1;
-  }
+  rect(x, y, boltiStaerd, boltiStaerd);
+	rect(mouseX,spadiY ,spadiBreidd,spadiThykkt);
+	fill(0);
+	text("Stig: " + stig,20,20);
 }
